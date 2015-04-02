2b22275e722bd1a7d8db5b134dbb44a64b502562
Aad Versteden <madnificent@gmail.com>
2015-04-03 01:28:59 +0200

    Extract Proxy from Dispatcher
    The Proxy module now takes care of shipping the request/response, the
    Dispatcher makes the connection and sends the correct request to the
    correct location.  The current contents are still mocked, but it should
    be fairly clear what an implementation of the Dispatcher will look
    like.
    
    If we assume Dockers in a future release, then we can start implementing
    the routes by base path and hostname.  With a minimal abstraction, we
    should then be able to tackle the dispatching in a declarative way.

---
+++ lib/proxy.ex
@@ -0,0 +1,46 @@
+defmodule Proxy do
+  import Plug.Conn
+
+  # Support for dispatching the connection
+  def send(conn, uri) do
+    # Start a request to the client saying we will stream the body.
+    # We are simply passing all req_headers forward.
+
+    {:ok, client} = :hackney.request(:get, uri, conn.req_headers, :stream, [])
+
+    conn
+    |> write_proxy(client)
+    |> read_proxy(client)
+  end
+
+  # Reads the connection body and write it to the
+  # client recursively.
+  defp write_proxy(conn, client) do
+    # Check Plug.Conn.read_body/2 docs for maximum body value,
+    # the size of each chunk, and supported timeout values.
+    case read_body(conn, []) do
+      {:ok, body, conn} ->
+        :hackney.send_body(client, body)
+        conn
+      {:more, body, conn} ->
+        :hackney.send_body(client, body)
+        write_proxy(conn, client)
+    end
+  end
+
+  # Reads the client response and sends it back.
+  defp read_proxy(conn, client) do
+    {:ok, status, headers, client} = :hackney.start_response(client)
+    {:ok, body} = :hackney.body(client)
+
+    # Delete the transfer encoding header. Ideally, we would read
+    # if it is chunked or not and act accordingly to support streaming.
+    #
+    # We may also need to delete other headers in a proxy.
+    headers = List.keydelete(headers, "Transfer-Encoding", 1)
+
+    %{conn | resp_headers: headers}
+    |> send_resp(status, body)
+  end
+
+end

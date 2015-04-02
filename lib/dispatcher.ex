0f59487ed0070ea7d4caaa12750c37ee3c28f153
Aad Versteden <madnificent@gmail.com>
2015-04-02 23:03:55 +0200

    Hello World with Cowboy and Plug
    Including the Cowboy and Plug dependencies and constructing a trivial
    hello world page with them.

--- lib/dispatcher.ex
+++ lib/dispatcher.ex
@@ -1,2 +1,12 @@
 defmodule Dispatcher do
+
+  def init( options ) do
+    options
+  end
+
+  def call(conn, _opts) do
+    conn
+    |> Plug.Conn.put_resp_content_type("text/plain")
+    |> Plug.Conn.send_resp( 200, "Hello World" )
+  end
 end
--- mix.exs
+++ mix.exs
@@ -12,7 +12,7 @@ defmodule Dispatcher.Mixfile do
   #
   # Type `mix help compile.app` for more information
   def application do
-    [applications: [:logger]]
+    [applications: [:cowboy, :plug, :logger]]
   end
 
   # Dependencies can be Hex packages:
@@ -25,6 +25,7 @@ defmodule Dispatcher.Mixfile do
   #
   # Type `mix help deps` for more examples and options
   defp deps do
-    []
+    [{:cowboy, "~> 1.0.0"},
+     {:plug, "~> 0.11.1"}]
   end
 end

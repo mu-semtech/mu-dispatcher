afc214d8d0866a9fbe30343dec0435ae6aae5759
Aad Versteden <madnificent@gmail.com>
2017-01-10 22:03:22 +0100

    Tag version 1.1.0
    New features:
    - downcased headers
    - streaming support
    
    Fixes since 1.0.0

--- mix.exs
+++ mix.exs
@@ -3,7 +3,7 @@ defmodule Dispatcher.Mixfile do
 
   def project do
     [app: :dispatcher,
-     version: "0.0.1",
+     version: "1.1.0",
      elixir: "~> 1.0",
      deps: deps,
      aliases: aliases]
